85e7db022a24ba4e1620dd9bc4213e1554b13e34
Manu Garcia <manu.garcia@accenture.com>
2017-11-30 23:11:07 +0100

    Initial commit

---
+++ src/main/groovy/info/developia/demogroovyreact/DemoGroovyReactApplication.groovy
@@ -0,0 +1,12 @@
+package info.developia.demogroovyreact
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DemoGroovyReactApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run DemoGroovyReactApplication, args
+	}
+}
---
+++ src/main/groovy/info/developia/demogroovyreact/controller/HomeController.groovy
@@ -0,0 +1,17 @@
+package info.developia.demogroovyreact.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HomeController {
+
+    @GetMapping("/")
+    def String home(Model model){
+
+        model.addAttribute("name", "This is Manu talking")
+
+        "home"
+    }
+}
